717f2f29b9736e595d58c096e2e38d841a14dfa0
santisolis97 <santux09@gmail.com>
2021-05-03 12:05:00 -0300

    google login ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Todos from "./routes/Todos";
+import SignIn from "./routes/SignIn";
+import PrivateRoute from "./components/PrivateRoute";
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>Redux Todo App</h1>
+      <Switch>
+        <PrivateRoute path="/todos">
+          <Todos />
+        </PrivateRoute>
+        <Route path="/">
+          <SignIn />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+import { rootReducer } from "./ducks/reducers";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFqCgkOFt_kFLW6ngqecKjLN18sEI0W-I",
+  authDomain: "reactreduxauth-f77be.firebaseapp.com",
+  projectId: "reactreduxauth-f77be",
+  storageBucket: "reactreduxauth-f77be.appspot.com",
+  messagingSenderId: "735614000736",
+  appId: "1:735614000736:web:a1ef4ca0156d011d816d05",
+  measurementId: "G-HM5ZY2FY90",
+};
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, //since we are using Firestore
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
